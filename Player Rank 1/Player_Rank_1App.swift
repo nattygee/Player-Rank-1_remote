01abc100d2de36d1d58b2aab8d7c189fd2e324a6
nattygee <83288489+nattygee@users.noreply.github.com>
2023-01-28 08:08:00 -0500

    Initial Commit

---
+++ Player Rank 1/Player_Rank_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Player_Rank_1App.swift
+//  Player Rank 1
+//
+//  Created by Tim Green on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct Player_Rank_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
