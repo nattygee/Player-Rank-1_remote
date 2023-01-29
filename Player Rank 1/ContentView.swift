fb2099f945162534c01fb7b1ef8c9ac96680f9f1
nattygee <83288489+nattygee@users.noreply.github.com>
2023-01-29 13:29:48 -0500

    • added a bunch of images to assets folder • mapped out the ui using containers and spacers • learning more about the modifiers - not as bad as when we were learning JS and css but def a bit of a new learning curve - the properties panel(?) (right) is pretty nifty to learn possible modifiers, but google is still my best friend

--- Player Rank 1/ContentView.swift
+++ Player Rank 1/ContentView.swift
@@ -9,28 +9,94 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            // comment here
-            Image(systemName: "globe")
-                .imageScale(.large)
+        VStack {
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
                 .foregroundColor(.accentColor)
-                .background(Color.green)
+            
+            Spacer()
+
+            Image("1HC_logo_full-05")
+                .resizable()
+                .padding(16)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 160, height: 100.0)
+            } .padding()
                 
             VStack {
-                Image("1HC_logo_full-05")
-                    .resizable()
-                    .padding(16)
-                    .aspectRatio(contentMode: .fit)
+                Spacer()
                 
-                Text("Testing this out")
-                    .fontWeight(.black)
-                    .padding(16)
-                    .background(Color.black)
-                    .foregroundColor(Color.white)
-                    .kerning(/*@START_MENU_TOKEN@*/2.01/*@END_MENU_TOKEN@*/)
+                // MARK: cards
+                HStack(spacing: 24.0) {
+                    Image("red01")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Image("blue01")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                .padding(24)
+                .background(Color.blue.opacity(0.1))
+                .cornerRadius(16.0)
+                
+                Spacer()
+                
+                // scores
+                VStack(spacing: 24.0) {
+                    Text("Scores")
+                        .font(.footnote)
+                        .foregroundColor(Color.gray)
+                        .textCase(.uppercase)
+                        
+                    HStack(spacing: 24.0) {
+                        Spacer()
+                        VStack() {
+                            Text("Player")
+                                .textCase(.uppercase)
+                                .padding(.bottom)
+                            Text("0")
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                        }
+                        VStack() {
+                            Text("Computer")
+                                .textCase(.uppercase)
+                                .padding(.bottom)
+                            Text("0")
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                        }
+                        Spacer()
+                    }
+                }
+                
+                Spacer()
+                
+                // MARK: buttons
+                VStack(spacing: 16.0) {
+                    Text("Get started")
+                        .fontWeight(.black)
+                        .padding(16)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .kerning(/*@START_MENU_TOKEN@*/2.01/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(8.0)
+                    
+                    Text("Maybe next time")
+                        .fontWeight(.black)
+                        .padding(16)
+                        .foregroundColor(Color.blue)
+                        .kerning(/*@START_MENU_TOKEN@*/2.01/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(8.0)
+                    .border(Color.blue, width: 2)
+                }
+
             }
+            Spacer()
         }
         .padding()
+        .background(Color.white)
     }
 }
 
