554642adb99a296e6718bf2ebf09b0aa161a4ec1
nattygee <83288489+nattygee@users.noreply.github.com>
2023-02-14 08:59:35 -0500

    • figuring out text layout using both spacers and frame alignment • also figured out how to make scroll views, holy

--- Player Rank 1/Moeykens Testing/CardTitlesView.swift
+++ Player Rank 1/Moeykens Testing/CardTitlesView.swift
@@ -11,115 +11,207 @@ struct CardTitlesView: View {
     var body: some View {
     
 
-        VStack(spacing: 16) {
-            Text("Card Titles Practice").font(.largeTitle).fontWeight(.black).multilineTextAlignment(.leading).frame(maxWidth: .infinity)
-            //individual card
-            VStack(alignment: .trailing) {
-                Image("macos_bg_yosemite")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 200)
-                    
-                // HOLY CONFUSING OVERLAY
-                    .overlay(
-                        HStack{
-                            Spacer()
-                            
-                            Text("Yosemite")
-                                .foregroundColor(.white)
-                                .font(.title3)
-                                .fontWeight(.bold)
-                            
-                            Image(systemName: "arrow.right")
-                                .foregroundColor(.white)
-                                .font(.title)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding([.bottom], 24)
-                            .padding([.top], 40)
-                            .padding(.horizontal)
-                            .background(
-                                LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
-                                               startPoint: .bottom,
-                                               endPoint: .top)
-                            )
-                        
-                        , alignment: .bottom
-                        )
-            }.cornerRadius(12).padding(.horizontal)
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Card Titles Practice").padding([.horizontal], 16).font(.largeTitle).fontWeight(.black).frame(maxWidth: .infinity, alignment: .leading)
             
-            //individual card
-            VStack(alignment: .trailing) {
-                Image("macos_bg_ventura")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 200)
-                    
-                // HOLY CONFUSING OVERLAY
-                    .overlay(
-                        HStack{
-                            Spacer()
-                            
-                            Text("Ventura")
-                                .foregroundColor(.white)
-                                .font(.title3)
-                                .fontWeight(.bold)
-                            
-                            Image(systemName: "arrow.right")
-                                .foregroundColor(.white)
-                                .font(.title)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding([.bottom], 24)
-                            .padding([.top], 40)
-                            .padding(.horizontal)
-                            .background(
-                                LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
-                                               startPoint: .bottom,
-                                               endPoint: .top)
-                            )
+            ScrollView {
+                
+                Rectangle().fill(Color.white.opacity(0))
+                Text("Mac Wallpapers").padding([.horizontal], 16).frame(maxWidth: .infinity, alignment: .leading).font(.title3).fontWeight(.bold).foregroundColor(Color.theme.primaryTextColor)
+                
+                //alignment using hstack + spacer
+                HStack {
+                    Text("Mac Wallpapers").padding([.horizontal], 16)
+                    Spacer()
+                }
+                
+                // alignment using frame width infinity + alignment
+                Text("Mac Wallpapers").padding([.horizontal], 16).frame(maxWidth: .infinity, alignment: .leading)
+                
+                Divider().padding([.bottom], 16)
+                //individual card
+                VStack(alignment: .trailing) {
+                    Image("macos_bg_snowleopard")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
                         
-                        , alignment: .bottom
-                        )
-            }.cornerRadius(12).padding(.horizontal)
+                    // HOLY CONFUSING OVERLAY
+                        .overlay(
+                            HStack{
+                                Spacer()
+                                
+                                Text("Snow Leopard")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding([.bottom], 24)
+                                .padding([.top], 40)
+                                .padding(.horizontal)
+                                .background(
+                                    LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
+                                                   startPoint: .bottom,
+                                                   endPoint: .top)
+                                )
+                            
+                            , alignment: .bottom
+                            )
+                }.cornerRadius(12).padding(.horizontal)
+                
+                //individual card
+                VStack(alignment: .trailing) {
+                    Image("macos_bg_cheetah")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
+                        
+                    // HOLY CONFUSING OVERLAY
+                        .overlay(
+                            HStack{
+                                Spacer()
+                                
+                                Text("Cheetah")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding([.bottom], 24)
+                                .padding([.top], 40)
+                                .padding(.horizontal)
+                                .background(
+                                    LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
+                                                   startPoint: .bottom,
+                                                   endPoint: .top)
+                                )
+                            
+                            , alignment: .bottom
+                            )
+                }.cornerRadius(12).padding(.horizontal)
+                
+                //individual card
+                VStack(alignment: .trailing) {
+                    Image("macos_bg_yosemite")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
+                        
+                    // HOLY CONFUSING OVERLAY
+                        .overlay(
+                            HStack{
+                                Spacer()
+                                
+                                Text("Yosemite")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding([.bottom], 24)
+                                .padding([.top], 40)
+                                .padding(.horizontal)
+                                .background(
+                                    LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
+                                                   startPoint: .bottom,
+                                                   endPoint: .top)
+                                )
+                            
+                            , alignment: .bottom
+                            )
+                }.cornerRadius(12).padding(.horizontal)
+                
+                //individual card
+                VStack(alignment: .trailing) {
+                    Image("macos_bg_ventura")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
+                        
+                    // HOLY CONFUSING OVERLAY
+                        .overlay(
+                            HStack{
+                                Spacer()
+                                
+                                Text("Ventura")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding([.bottom], 24)
+                                .padding([.top], 40)
+                                .padding(.horizontal)
+                                .background(
+                                    LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
+                                                   startPoint: .bottom,
+                                                   endPoint: .top)
+                                )
+                            
+                            , alignment: .bottom
+                            )
+                }.cornerRadius(12).padding(.horizontal)
+                
+                //individual card
+                VStack(alignment: .trailing) {
+                    Image("macos_bg_monterey")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 200)
+                        
+                    // HOLY CONFUSING OVERLAY
+                        .overlay(
+                            HStack{
+                                Spacer()
+                                
+                                Text("Monterey")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.white)
+                                    .font(.title)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding([.bottom], 24)
+                                .padding([.top], 40)
+                                .padding(.horizontal)
+                                .background(
+                                    LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
+                                                   startPoint: .bottom,
+                                                   endPoint: .top)
+                                )
+                            
+                            , alignment: .bottom
+                            )
+                }.cornerRadius(12).padding(.horizontal)
+            }
+            //.padding([.top], 24)
             
-            //individual card
-            VStack(alignment: .trailing) {
-                Image("macos_bg_monterey")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 200)
-                    
-                // HOLY CONFUSING OVERLAY
-                    .overlay(
-                        HStack{
-                            Spacer()
-                            
-                            Text("Monterey")
-                                .foregroundColor(.white)
-                                .font(.title3)
-                                .fontWeight(.bold)
-                            
-                            Image(systemName: "arrow.right")
-                                .foregroundColor(.white)
-                                .font(.title)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding([.bottom], 24)
-                            .padding([.top], 40)
-                            .padding(.horizontal)
-                            .background(
-                                LinearGradient(colors: [.black.opacity(0.45), .black.opacity(0)],
-                                               startPoint: .bottom,
-                                               endPoint: .top)
-                            )
-                        
-                        , alignment: .bottom
-                        )
-            }.cornerRadius(12).padding(.horizontal)
+            
         }
     }
 }
